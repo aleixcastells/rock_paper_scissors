d98540378abf874545e2a700cc87aed2abe5aecf
aleixcastells <castells.94@gmail.com>
2023-02-04 21:13:18 +0100

    Features update
    Added features
    
    - Random selection
    - Player name functionality
    - Player colour functionality
    - Result page design
    - Single player mode

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 const GAME = {
     players: [],
+    cpu_player: false,
     total_rounds: 0,
     current_round: 1,
     selected_weapons: 0,
@@ -9,10 +10,11 @@ const GAME = {
 
 class Player {
     constructor(player_number) {
-        this.number = player_number,
-            this.weapon = '',
-            this.colour = GAME.player_colours[player_number - 1],
-            this.points = [0, 0]
+        this.number = player_number
+        this.player_name = 'Player ' + String(player_number)
+        this.weapon = ''
+        this.colour = GAME.player_colours[player_number - 1]
+        this.points = [0, 0]
     }
 }
 
@@ -26,20 +28,22 @@ function showScreen(screen_name = 'select_players_screen') {
 
 function screenManager(button, number) {
 
-    console.log(button)
-
-    if (button == 'rounds_amount' || button == 'select_weapon' || button == 'next_round') {
-        //    updateWeaponScreen()
-
-
-
-    }
+    console.log(number)
 
     switch (button) {
 
+
+
         case 'players_amount':
 
-            createPlayers(number)
+            if (number == 1) {
+                createPlayers(2)
+                GAME.cpu_player = true
+
+            }
+
+            if (number > 1) { createPlayers(number) }
+
             showScreen('select_rounds_screen')
             break
 
@@ -51,14 +55,25 @@ function screenManager(button, number) {
 
         case 'select_weapon':
 
+            if (GAME.players.length == 2 && GAME.cpu_player == true) {
+                selectWeapon(number)
+                randomWeapon()
+                GAME.players[1].player_name = 'CPU'
 
-            selectWeapon(number)
+            }
+            if (GAME.cpu_player == false) {
+                selectWeapon(number)
+            }
+
             if (GAME.selected_weapons == GAME.players.length) { showScreen('go_screen') }
+
             break;
     }
+
+    setPlayerName(button)
 }
 
-function nextRound(next_round) {
+function nextRound() {
 
     screenManager('next_round', 0)
 
@@ -90,6 +105,7 @@ function createPlayers(player_amount) {
 }
 
 function selectWeapon(clicked_weapon) {
+
     GAME.players[GAME.selected_weapons].weapon = clicked_weapon
     GAME.selected_weapons++
 }
@@ -138,14 +154,12 @@ function addPoints(i, player, loosing_weapon) {
         else { return 0 }
     }
     if (GAME.players.length > 3) {
-        if (GAME.players[i].weapon == loosing_weapon) { // && player.points[0] != GAME.current_round
+        if (GAME.players[i].weapon == loosing_weapon) {
             player.points[1] += 1
             return 1
         }
         else { return 0 }
     }
-
-
 }
 
 function showWinner() {
@@ -154,9 +168,9 @@ function showWinner() {
     let highest = Math.max(...GAME.scoreboard)
 
     GAME.players.forEach(function (player) {
-        scoreboard_string += `Player ${player.number}: (${player.points[1]}) ${player.weapon} <br>`
+        scoreboard_string += `${player.player_name}: (${player.points[1]}) ${player.weapon} <br>`
         if (player.points[1] == highest) {
-            winners_string += `<span id=\"winner${player.number}" class="winner" style="color: ${player.colour}">Player ${player.number}</span><br>`
+            winners_string += `<span id=\"winner${player.number}" class="winner" style="color: ${player.colour}">${player.player_name}</span><br>`
         }
     })
 
@@ -169,47 +183,59 @@ function hideNextRound() {
     else { return false }
 }
 
+function setPlayerName(button) {
+
+    if (GAME.selected_weapons == GAME.players.length) {
+        document.body.style.setProperty('background-color', 'black', 'important')
+        return
+    }
+
+    if (button == 'rounds_amount' || button == 'next_round') {
+
+        let player_colour = `${GAME.players[GAME.selected_weapons].colour}`
+        document.body.style.setProperty('background-color', player_colour, 'important')
+
+        document.getElementById('player_name').innerHTML = GAME.players[GAME.selected_weapons].player_name
+    }
+
+    if (button == 'select_weapon') {
+
+        let player_colour = `${GAME.players[GAME.selected_weapons].colour}`
+        document.body.style.setProperty('background-color', player_colour, 'important')
+
+        document.getElementById('player_name').innerHTML = GAME.players[GAME.selected_weapons].player_name
 
 
-
-
-function setPlayerName() {
+    }
 }
-
-
-
-
-
-
-
 
 function randomWeapon() {
-    screenManager('select_weapon', 'rock')
+
+    let random_number = Math.ceil(Math.random() * 3)
+    console.log(random_number)
+    let button = random_number == 1 ? 'rock' : random_number == 2 ? 'paper' : 'scissors'
+
+    selectWeapon(button)
+    setPlayerName('select_weapon')
+
+    if (GAME.selected_weapons == GAME.players.length) { showScreen('go_screen') }
 }
 
 
 
-/*
-function updateWeaponScreen() {
-
-    if (GAME.round_clicks == GAME.players.length) {
-        document.body.style.setProperty('background-color', 'black', 'important');
-        GAME.round_clicks = 0
-    }
-    else {
-        let player_colour = `${GAME.players[GAME.round_clicks].colour}`
-        document.body.style.setProperty('background-color', player_colour, 'important')
-    }
-}
-
-*/
-
-
 
 /*
-- Single player mode
-- Random selection
+
+TO DO:
 - Maybe custom names?
+
+
+DONE:
+- Random selection
+- Player name functionality
 - Player colour functionality
 - Result page design
+- Single player mode
+
 */
+
